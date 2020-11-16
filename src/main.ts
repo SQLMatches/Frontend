8a3d2134c8305fd5c4b16d030eaa6e28fc2923a4
Ward <wardpearce@protonmail.com>
2020-11-16 19:16:30 +1300

    Moving back to vue 2

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import './assets/css/bootstrap.min.css'
-import './assets/css/styles.css'
-import './assets/fonts/line-awesome.min.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
