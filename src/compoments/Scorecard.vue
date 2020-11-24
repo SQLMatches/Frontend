75ef409fb6b35fb5a572cc1d3d9a17619fb022d4
Ward <wardpearce@protonmail.com>
2020-11-24 19:00:34 +1300

    Scoreboard shows players

---
+++ src/compoments/Scorecard.vue
@@ -0,0 +1,40 @@
+<template>
+  <table class="table table-striped table-bordered table-hover table-dark">
+      <thead>
+          <tr>
+              <th class="name-col">Name</th>
+              <th>Kills</th>
+              <th>Deaths</th>
+              <th>Assists</th>
+              <th>KDR</th>
+              <th>HS %</th>
+              <th>Hit %</th>
+              <th>MVPs</th>
+              <th>Score</th>
+              <th>Ping</th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr v-for="(player, index) in team" :key="index">
+              <td>{{ player.name }}</td>
+              <td>{{ player.kills }}</td>
+              <td>{{ player.deaths }}</td>
+              <td>{{ player.assists }}</td>
+              <td>{{ player.kdr }}</td>
+              <td>{{ player.hs_percentage }}%</td>
+              <td>{{ player.hit_percentage }}%</td>
+              <td>{{ player.mvps }}</td>
+              <td>{{ player.score }}</td>
+              <td>{{ player.ping }}</td>
+          </tr>
+      </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: [
+    'team'
+  ]
+}
+</script>
--- src/pages/Scoreboard.vue
+++ src/pages/Scoreboard.vue
@@ -5,173 +5,19 @@
                 <div class="card-body">
                     <h2 class="text-center text-light">Team {{ scoreboard.team_1_name }}</h2>
                     <div class="table-responsive table-borderless">
-                        <table class="table table-striped table-bordered table-hover table-dark">
-                            <thead>
-                                <tr>
-                                    <th class="name-col">Name</th>
-                                    <th>Kills</th>
-                                    <th>Deaths</th>
-                                    <th>Assists</th>
-                                    <th>KDR</th>
-                                    <th>HS %</th>
-                                    <th>Hit %</th>
-                                    <th>MVPs</th>
-                                    <th>Score</th>
-                                    <th>Ping</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Ward</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0.0</td>
-                                    <td>0%</td>
-                                    <td>0%</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                </tr>
-                                <tr>
-                                    <td>Ward</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0.0</td>
-                                    <td>0%</td>
-                                    <td>0%</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                </tr>
-                                <tr>
-                                    <td>Ward</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0.0</td>
-                                    <td>0%</td>
-                                    <td>0%</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                </tr>
-                                <tr>
-                                    <td>Ward</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0.0</td>
-                                    <td>0%</td>
-                                    <td>0%</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                </tr>
-                                <tr>
-                                    <td>Ward</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0.0</td>
-                                    <td>0%</td>
-                                    <td>0%</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                      <scorecard :team="scoreboard.team_1"></scorecard>
                     </div>
                 </div>
             </div>
             <div class="text-center" style="margin-bottom: 20px;margin-top: 20px;">
                 <h1 class="text-light">{{ scoreboard.team_1_score }}:{{ scoreboard.team_2_score }}</h1>
-                <button class="btn btn-primary btn-sm" type="button" style="margin-top: 8px;">&nbsp;Download&nbsp;<i class="la la-download"></i></button>
+                <button class="btn btn-primary btn-sm" type="button" style="margin-top: 8px;">&nbsp;Download&nbsp;<b-icon icon="download" variant="light"></b-icon></button>
             </div>
             <div class="card bg-dark content-div">
                 <div class="card-body">
                     <h2 class="text-center text-light">Team {{ scoreboard.team_2_name }}</h2>
                     <div class="table-responsive table-borderless">
-                        <table class="table table-striped table-bordered table-hover table-dark">
-                            <thead>
-                                <tr>
-                                    <th class="name-col">Name</th>
-                                    <th>Kills</th>
-                                    <th>Deaths</th>
-                                    <th>Assists</th>
-                                    <th>KDR</th>
-                                    <th>HS %</th>
-                                    <th>Hit %</th>
-                                    <th>MVPs</th>
-                                    <th>Score</th>
-                                    <th>Ping</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Ward</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0.0</td>
-                                    <td>0%</td>
-                                    <td>0%</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                </tr>
-                                <tr>
-                                    <td>Ward</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0.0</td>
-                                    <td>0%</td>
-                                    <td>0%</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                </tr>
-                                <tr>
-                                    <td>Ward</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0.0</td>
-                                    <td>0%</td>
-                                    <td>0%</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                </tr>
-                                <tr>
-                                    <td>Ward</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0.0</td>
-                                    <td>0%</td>
-                                    <td>0%</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                </tr>
-                                <tr>
-                                    <td>Ward</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0.0</td>
-                                    <td>0%</td>
-                                    <td>0%</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                    <td>0</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                      <scorecard :team="scoreboard.team_2"></scorecard>
                     </div>
                 </div>
             </div>
@@ -182,8 +28,13 @@
 <script>
 import axios from 'axios'
 
+import Scorecard from '../compoments/Scorecard.vue'
+
 export default {
   name: 'Scoreboard',
+  components: {
+    Scorecard
+  },
   data () {
     return {
       scoreboard: {}
