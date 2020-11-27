3fe9d959b97e38ed1df6296853a822c56f510d5d
Ward <wardpearce@protonmail.com>
2020-11-27 16:53:53 +1300

    Started flow of create community page, create community button hides on nav once on page

--- src/pages/Create.vue
+++ src/pages/Create.vue
@@ -7,9 +7,9 @@
               <div class="steps-progressbar">
                   <ul>
                       <li class="previous">Start</li>
-                      <li class="active">Community details</li>
-                      <li>Setting up servers</li>
-                      <li>Finished</li>
+                      <li v-bind:class="{'active':stepCounter === 0, 'previous': stepCounter > 0}" v-on:click="setStepValue(0)" style="cursor: pointer;">Community details</li>
+                      <li v-bind:class="{'active':stepCounter === 1, 'previous': stepCounter > 1}" v-on:click="setStepValue(1)" style="cursor: pointer;">Setting up servers</li>
+                      <li v-bind:class="{'active':stepCounter === 2, 'previous': stepCounter > 2}" v-on:click="setStepValue(2)" style="cursor: pointer;">Finished</li>
                   </ul>
               </div>
               <!-- End: Steps Progressbar -->
@@ -17,7 +17,13 @@
       </div>
       <div class="card bg-dark content-div">
           <div class="card-body">
-              <form></form><button class="btn btn-info btn-block btn-lg" type="button">NEXT&nbsp;<i class="la la-chevron-right"></i></button></div>
+            <div v-if="stepCounter === 0">
+              <b-form-input placeholder="Community name"></b-form-input>
+            </div>
+
+            <button v-if="stepCounter < 2" v-on:click="stepCounter++" class="btn btn-info btn-block btn-lg" type="button">Next&nbsp;<b-icon icon="chevron-double-right" variant="light"></b-icon></button>
+            <button v-else class="btn btn-info btn-block btn-lg" type="button">Create community&nbsp;<b-icon icon="hand-thumbs-up" variant="light"></b-icon></button>
+          </div>
       </div>
     </div>
       <div v-else class="card bg-dark content-div">
@@ -38,13 +44,24 @@ export default {
   name: 'Create',
   data () {
     return {
-      communityName: null
+      communityName: null,
+      stepCounter: 0,
+      steamID: null
     }
   },
   async created () {
+    this.steamID = localStorage.getItem('steamID')
+
     await axios.get('/community/').then(res => {
       this.communityName = res.data.data.community_name
     })
+  },
+  methods: {
+    setStepValue (value) {
+      this.stepCounter = value
+    },
+    async createCommunity () {
+    }
   }
 }
 </script>
