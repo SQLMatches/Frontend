bb7c917e215f7837c69e38a8d74c009c7b32034c
Ward <wardpearce@protonmail.com>
2021-01-02 20:51:46 +1300

    Update Communities.vue

--- src/components/Communities.vue
+++ src/components/Communities.vue
@@ -5,8 +5,7 @@
           <router-link :to="{name: 'CommunityPage', params: {'communityName': community.community_name}}">
             <div class="card light team">
                 <div class="card-body text-center">
-                  <img class="img-thumbnail team-pfp" src="@/assets/img/ahahha.png">
-                    <h4 class="text-light card-title">{{ community.community_name }}</h4>
+                  <h4 class="text-light card-title" style="margin-bottom:0;">{{ community.community_name }}</h4>
                 </div>
             </div>
           </router-link>
